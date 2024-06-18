f1a7466b4e8855d7e6608eaf2c536cd404aaa2ac
Joe Kim <joesm.genie@gmail.com>
2024-06-18 11:20:02 +0700

    Update function regional method post

--- pages/api/regional.ts
+++ pages/api/regional.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+type PostBody = {
+  ip: string;
+};
+
 type Data = {
   ip: string;
   sum: number;
@@ -10,21 +14,32 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'GET') {
-    const body = {
-      msg: `I am an Edge function! (executed on ${process.env.VERCEL_REGION})`,
-    };
-    return res.status(200).json(body);
-  }
+  try {
+    if (req.method === 'GET') {
+      const body = {
+        msg: `I am an Edge function! (executed on ${process.env.VERCEL_REGION})`,
+      };
+      return res.status(200).json(body);
+    }
 
-  if (req.method === 'POST') {
-    const ip = req.body.ip;
-    const sum = ip.split('.').reduce((acc: number, val: string) => acc + parseInt(val), 0);
-    const response: Data = {
-      ip,
-      sum,
-      msg: `I am an Edge function! (executed on ${process.env.VERCEL_REGION}) and return the sum of the IP address`,
-    };
-    return res.status(200).json(response);
+    if (req.method === 'POST') {
+      const body = JSON.parse(req.body) as PostBody;
+      const ip = body.ip;
+      if (!ip) {
+        return res.status(400).json({ msg: 'IP address is required!' });
+      }
+      const sum = ip.split('.').reduce((acc: number, val: string) => acc + parseInt(val), 0);
+      const response: Data = {
+        ip,
+        sum,
+        msg: `I am an Edge function! (executed on ${process.env.VERCEL_REGION}) and return the sum of the IP address`,
+      };
+      return res.status(200).json(response);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ msg: error.message });
+    }
+    res.status(500).end();
   }
 }
