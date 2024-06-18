6f12412f84ed607b89cdb2707dd0efff33444d55
Joe Kim <joesm.genie@gmail.com>
2024-06-18 11:42:23 +0700

    Update middleware to check secret path in regional page

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { SECRET_URL, specialChars } from './constant';
 
+export const config = {
+  runtime: 'experimental-edge',
+};
+
 export default function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
 
-  const { geo, ip } = request;
+  const { ip, geo, nextUrl: url } = request;
   const country = geo?.country || 'Unknown';
   const city = geo?.city || 'Unknown';
   requestHeaders.set('x-country', country);
   requestHeaders.set('x-ip', ip || '127.0.0.1');
 
-  const x = specialChars.filter((char) => {
-    if (ip?.includes(char.toString())) {
-      return char;
-    }
-  });
+  if (url.pathname.startsWith('/regional')) {
+    const x = specialChars.filter((char) => {
+      if (ip?.includes(char.toString())) {
+        return char;
+      }
+    });
 
-  if (x.length > 0) {
-    return NextResponse.redirect(SECRET_URL);
+    if (x.length > 0) {
+      return NextResponse.redirect(SECRET_URL);
+    }
   }
 
   if (country === 'TH' && city === 'Bangkok') {
