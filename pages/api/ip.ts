04b54be9ca0a314b8363975ec2fe2e87d3076764
Joe Kim <joesm.genie@gmail.com>
2024-06-18 16:13:11 +0700

    Change deploy site to sao paolo

---
+++ pages/api/ip.ts
@@ -0,0 +1,26 @@
+import { NextRequest } from 'next/server';
+
+export const runtime = 'edge';
+
+export default async function handler(
+  req: NextRequest,
+) {
+  try {
+    if (req.method === 'GET') {
+      const ip = req.headers.get('x-ip');
+      const sum = ip?.split('.').reduce((acc: number, val: string) => acc + parseInt(val), 0);
+      const body = {
+        ip,
+        sum,
+        msg: `I am an Edge function! (executed on ${process.env.VERCEL_REGION})`,
+      };
+      return new Response(JSON.stringify(body), { status: 200 });
+    }
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      return new Response(JSON.stringify({ msg: error.message }), { status: 500 });
+    }
+    return new Response(JSON.stringify({ msg: 'Unknown error has occured!' }), { status: 500 });
+  }
+}
