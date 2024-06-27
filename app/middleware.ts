9f939e418a8a9d7288bd6621daef2c72875402e5
Joe Kim <joesm.genie@gmail.com>
2024-06-27 15:57:00 +0700

    Add middleware

---
+++ app/middleware.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from 'next/server';
+// import { SECRET_URL, specialChars } from './constant';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+
+  const { geo, nextUrl: url } = request;
+  const country = geo?.country || 'Unknown';
+  const city = geo?.city || 'Unknown';
+  requestHeaders.set('x-country', country);
+
+  // if (url.pathname.startsWith('/regional')) {
+  //   const x = specialChars.filter((char) => {
+  //     if (ip?.includes(char.toString())) {
+  //       return char;
+  //     }
+  //   });
+
+  //   if (x.length > 0) {
+  //     return NextResponse.redirect(SECRET_URL);
+  //   }
+  // }
+
+  if (country === 'TH' && city === 'Bangkok') {
+    requestHeaders.set('x-special-msg', 'show');
+  }
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  return response;
+}
