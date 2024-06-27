ff5c830965e60a28e25fa55f18c0212b7cb6b97f
Joe Kim <joesm.genie@gmail.com>
2024-06-27 16:36:53 +0700

    Add constant to check lucky number

--- app/constant.ts
+++ app/constant.ts
@@ -1,3 +1,5 @@
+export const specialChars = [82, 73, 67, 75, 82, 79, 76, 76];
+export const SECRET_URL = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 export const translations: { [key: string]: string } = {
   UNKNOWN: 'Hello!',
   CA: 'Hello!',
