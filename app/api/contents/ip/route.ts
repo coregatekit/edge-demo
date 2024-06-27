6d384554b282bc074ab613ac210b046249189571
Joe Kim <joesm.genie@gmail.com>
2024-06-27 14:40:28 +0700

    Update function calculate ip

--- app/api/contents/ip/route.ts
+++ app/api/contents/ip/route.ts
@@ -1,3 +1,21 @@
 export function GET(request: Request) {
-  return new Response(`Hello ip from ${process.env.VERCEL_REGION}`);
+  console.log(request.headers);
+  const ip = request.headers.get('x-forwarded-for');
+
+  if (!ip) {
+    return new Response(JSON.stringify({ msg: 'Unable to get ip' }), { status: 400 });
+  }
+
+  const ips = ip.split('.');
+  if (ips.length !== 4) {
+    return new Response(JSON.stringify({ msg: 'Unable to get ip' }), { status: 400 });
+  }
+
+  const sum = ips.reduce((acc, val) => acc + parseInt(val), 0);
+  const body = {
+    ip,
+    sum,
+    msg: `Hello, I am vercel function executed from ${process.env.VERCEL_REGION}`,
+  };
+  return new Response(JSON.stringify(body), { status: 200 });
 }
